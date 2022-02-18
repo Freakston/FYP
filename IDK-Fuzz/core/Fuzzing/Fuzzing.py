2515aa60a8a5fb76943707b061f44ff2fc297358
Aditya <padalaadityavardhan@gmail.com>
2022-02-18 16:39:12 +0000

    core/Fuzzing.py: basic harness added

--- IDK-Fuzz/core/Fuzzing/Fuzzing.py
+++ IDK-Fuzz/core/Fuzzing/Fuzzing.py
@@ -1,12 +1,68 @@
+from email import message
 import json
+import os 
+import ctypes
 from core.Utils.utils import rabConnect
 from base64 import b64decode
+
+class PosixSpawn():
+        def __init__(self):
+                self.libc = ctypes.cdll.LoadLibrary("libc.so.6")
+                self._posix_spawn = self.libc.posix_spawn
+                self._posix_spawn.restype = ctypes.c_int
+                self._posix_spawn.argtypes = (
+                        ctypes.POINTER(ctypes.c_int),
+                        ctypes.c_char_p, ctypes.c_void_p, ctypes.c_void_p,
+                        ctypes.POINTER(ctypes.c_char_p),
+                        ctypes.POINTER(ctypes.c_char_p)
+                )
+                # dirty hack: hardcoded struct sizes
+                self.attrs = self.libc.malloc(336)
+                self.actions = self.libc.malloc(80)
+                self.devnull = open("/dev/null","wb")
+                self.env = [x+"="+os.environ[x] for x in os.environ] + [ 0 ]
+
+        def execute(self, exe, args):
+                pid = ctypes.c_int()
+                args = [exe] + args + [ 0 ]
+                argv = (ctypes.c_char_p * 5) (*args)
+                env = (ctypes.c_char_p * ( len(self.env) ))(*self.env)
+                self.libc.posix_spawnattr_init(self.attrs)
+                self.libc.posix_spawnattr_setflags(self.attrs, 0x40)
+                self.libc.posix_spawn_file_actions_init(self.actions)
+                self.libc.posix_spawn_file_actions_adddup2(self.actions, self.devnull.fileno(), 1)
+                self._posix_spawn(ctypes.byref(pid), ctypes.c_char_p(exe),
+                        self.actions, self.attrs,
+                        ctypes.cast(argv, ctypes.POINTER(ctypes.c_char_p)),
+                        ctypes.cast(env, ctypes.POINTER(ctypes.c_char_p)))
+                status = ctypes.c_int()
+                self.libc.waitpid(pid.value, ctypes.byref(status), 0)
+
 class Fuzzer():
     def __init__(self) -> None:
         self.channel = rabConnect()
-    
-    def run(self, app):
-        print("Implement run Fuzzing")
+        self.ps = PosixSpawn()
+
+    def run_posix(self,message):
+        # Assuming the struct of the message as following
+        # message = {exe : <executable_name>; input : <input>}
+        exe = message["exe"]
+        inp = message["input"]
+        self.ps.execute(exe,inp)
+
+    def run_subprocess(self, message):
+        cmd = message["exe"]
+        inp = message["input"]
+        try: 
+            op = subprocess.run([f"{cmd}"], check=True,universal_newlines = True, stdout = subprocess.PIPE)
+
+        except subprocess.CalledProcessError as op:
+            print(op)
+            print(f"Process unexpected exit {op.returncode}")
+            f = open(f"crashes/crash_{abs(op.returncode)}_{time.time()}","w")
+            f.write(inp)
+            f.close()
+            exit()
 
     def blob_consumer(ch, method, properties, message):
         try:
@@ -21,7 +77,7 @@ class Fuzzer():
         # Connect to the result queue - consumer
         self.channel.basic_qos(prefetch_count=1)
         self.channel.basic_consume(
-            queue="task-queue", on_message_callback=blob_consumer)
+            queue="task-queue", on_message_callback=self.blob_consumer)
 
         self.channel.start_consuming()
 
